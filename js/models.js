3a571097da64a584c9c49f71b41728787dc5d33e
David Mosher <davidmosher@gmail.com>
2011-05-08 11:27:11 -0400

    scaling up metric data

--- js/models.js
+++ js/models.js
@@ -1,4 +1,4 @@
-(function($, B) {
+(function($, BIG) {
   
   BIG.Models = {
     Country: Backbone.Model.extend({}),
@@ -20,16 +20,16 @@
       model: BIG.Models.Result,
       localStorage: new Store("results")
     }),
-    Metrics: Backbone.Collection.extend({
+    MetricData: Backbone.Collection.extend({
       model: BIG.Models.Metric,
       localStorage: new Store("metrics")
     })
   };
    
-  BIG.Countries   = new BIG.Collections.Countries;
-  BIG.Companies   = new BIG.Collections.Companies;
-  BIG.Results     = new BIG.Collections.Results;
-  BIG.Metrics     = new BIG.Collections.Metrics;
+  BIG.Countries      = new BIG.Collections.Countries;
+  BIG.Companies      = new BIG.Collections.Companies;
+  BIG.Results        = new BIG.Collections.Results;
+  BIG.MetricData     = new BIG.Collections.MetricData;
   
   BIG._loadData = function() {
     _.each(
@@ -40,13 +40,52 @@
         $.getJSON('static-data/wb.' + dataType.collection.toLowerCase() + '.json', function(response) {
           _.each(response, function(entityData) {
             var entity = new dataType.model(entityData);
-            B[dataType.collection].add(entity);
+            BIG[dataType.collection].add(entity);
             entity.save();
           });
         });      
       }
     );
-  }
+  };
+  
+  BIG._loadMetrics = function() {
+    _.each(
+      [
+        { collection: "MetricData"  , model: BIG.Models.Metric, name: "GDP"  },
+        { collection: "MetricData"  , model: BIG.Models.Metric, name: "GNI"  }
+      ],
+      function(metric) {
+        
+        $.getJSON('static-data/wb.' + metric.name.toLowerCase() + '.json', function(response) {
+          _.each(response, function(rawMetricData) {
+            // there _has_ to be a more efficient way to do this :P
+            var transposed = {
+              id: _(rawMetricData[1]).chain().pluck('country').pluck('id').uniq().value()[0] + ":" + metric.name,
+              indicator: {
+                id: _(rawMetricData[1]).chain().pluck('indicator').pluck('id').uniq().value()[0],
+                value: _(rawMetricData[1]).chain().pluck('indicator').pluck('value').uniq().value()[0]
+              },
+              country: {
+                id: _(rawMetricData[1]).chain().pluck('country').pluck('id').uniq().value()[0],
+                value: _(rawMetricData[1]).chain().pluck('country').pluck('value').uniq().value()[0]
+              },
+              data: _.map(rawMetricData[1], function(raw) {
+                return {
+                  date: raw.date,
+                  decimal: raw.decimal,
+                  value: raw.value
+                }
+              })
+            };          
+
+            var entity = new metric.model(transposed);
+            BIG[metric.collection].add(entity);
+            entity.save();
+          });
+        });
+      }
+    );
+  };
   
   BIG._clearData = function() {
     window.localStorage.clear();
