ee5625bf125602c10df3c1b87f0fe2ee03f3638f
David Mosher <davidmosher@gmail.com>
2021-11-24 18:11:52 -0500

    leaflet map

--- js/views.js
+++ js/views.js
@@ -22,11 +22,19 @@
                 type: 'Country'
             }));
             if(country.latitude && country.longitude) {
-              new google.maps.Map($("#map")[0], {
-                zoom: 6,
-                center: new google.maps.LatLng(parseFloat(country.latitude, 10), parseFloat(country.longitude, 10)),
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-              });
+              var map = L.map('map').setView([
+                  parseFloat(country.latitude),
+                  parseFloat(country.longitude)
+              ], 13)
+
+              L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+                  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                  maxZoom: 18,
+                  id: 'mapbox/streets-v11',
+                  tileSize: 512,
+                  zoomOffset: -1,
+                  accessToken: 'pk.eyJ1IjoiZG1vc2hlciIsImEiOiJjamxlamt1dG0wM2I1M3BwMWJ2eDBweWE1In0.u4EpQKzUyH7AIzpD0J_P7g'
+              }).addTo(map);
             }
         }
 
